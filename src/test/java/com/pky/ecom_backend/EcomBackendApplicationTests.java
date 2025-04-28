1bd76872af67c7d6b98e69f0efc9f2b1bf22a531
Prabhat Kumar <k.prabhat1164@gmail.com>
2025-04-28 20:02:00 +0530

    initial commit

---
+++ src/test/java/com/pky/ecom_backend/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pky.ecom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
