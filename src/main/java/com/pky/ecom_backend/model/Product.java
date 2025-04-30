147ed6f4d9b51fd364fdbf1e3ec7ad9e545b29bc
Prabhat Kumar <k.prabhat1164@gmail.com>
2025-04-30 11:06:34 +0530

    Model updated

--- src/main/java/com/pky/ecom_backend/model/Product.java
+++ src/main/java/com/pky/ecom_backend/model/Product.java
@@ -1,18 +1,35 @@
 package com.pky.ecom_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String description;
     private String brand;
     private BigDecimal price;
     private String category;
-    private Data releaseDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
 
